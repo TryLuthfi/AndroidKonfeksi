50df0d2fe172c4b5b543a35d110fb5da5e70560a
TryLuthfi <ty01091990@gmail.com>
2019-05-16 12:44:44 +0700

    tambah penjualan

---
+++ app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/DashBoard.java
@@ -0,0 +1,13 @@
+package indonesia.konfeksi.com.androidkonfeksi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
--- app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/TambahPenjualan.java
+++ app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/TambahPenjualan.java
@@ -3,11 +3,11 @@ package indonesia.konfeksi.com.androidkonfeksi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TambahPenjualan extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tambah_penjualan);
     }
 }
