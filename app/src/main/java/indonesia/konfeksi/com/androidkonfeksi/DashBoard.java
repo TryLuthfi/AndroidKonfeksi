6a62f5d9226d7a6d6a0dafdde60a72c093f53de0
TryLuthfi <ty01091990@gmail.com>
2019-05-16 15:11:16 +0700

    tambah penjualan

--- app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/DashBoard.java
+++ app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/DashBoard.java
@@ -1,13 +1,26 @@
 package indonesia.konfeksi.com.androidkonfeksi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class DashBoard extends AppCompatActivity {
+    private LinearLayout penjualan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+
+        penjualan = findViewById(R.id.penjualan);
+        penjualan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TambahPenjualan.class);
+                startActivity(intent);
+            }
+        });
     }
 }
