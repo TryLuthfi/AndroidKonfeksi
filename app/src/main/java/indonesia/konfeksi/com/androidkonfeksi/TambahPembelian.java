351ffe0ab12a85b61520cbe1fc0eac1b7eb3c5eb
AlifFahreza <48142180+AlifFahreza@users.noreply.github.com>
2019-05-16 15:11:34 +0700

    fix

---
+++ app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/TambahPembelian.java
@@ -0,0 +1,13 @@
+package indonesia.konfeksi.com.androidkonfeksi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TambahPembelian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_pembelian);
+    }
+}
