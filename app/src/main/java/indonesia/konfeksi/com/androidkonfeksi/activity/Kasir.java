a31a6566ed294a7cbf5426b3c568d47e00a8e19a
= <fatikhunnizam@gmail.com>
2019-06-29 12:38:44 +0700

    adding some page

---
+++ app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/activity/Kasir.java
@@ -0,0 +1,15 @@
+package indonesia.konfeksi.com.androidkonfeksi.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import indonesia.konfeksi.com.androidkonfeksi.R;
+
+public class Kasir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kasir);
+    }
+}
