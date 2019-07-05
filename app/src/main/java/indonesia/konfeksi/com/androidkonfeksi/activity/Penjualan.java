bac6e9e5b4dcc4116de58c1f05f1135fd5603c91
AlifFahreza <48142180+AlifFahreza@users.noreply.github.com>
2019-07-05 12:50:30 +0700

    penjualan layout, penjualan java

--- app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/activity/Penjualan.java
+++ app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/activity/Penjualan.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -18,6 +19,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import indonesia.konfeksi.com.androidkonfeksi.R;
 import indonesia.konfeksi.com.androidkonfeksi.adapter.ProductsAdapter;
 import indonesia.konfeksi.com.androidkonfeksi.json.Product;
@@ -25,11 +30,36 @@ import indonesia.konfeksi.com.androidkonfeksi.konfigurasi.konfigurasi;
 
 public class Penjualan extends AppCompatActivity {
     private static final String TAG = "Penjualan";
+    private String date, time;
+    private EditText input_tgl_nota, input_waktu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_penjualan);
+
+        input_tgl_nota = findViewById(R.id.input_tgl_nota);
+        input_waktu = findViewById(R.id.input_waktu);
+
+        setDate();
+        setTime();
+
+        input_tgl_nota.setText(date);
+        input_waktu.setText(time);
     }
+
+    public void setDate() {
+        Date today = Calendar.getInstance().getTime();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy");
+        date = formatter.format(today);
+    }
+
+    public void setTime(){
+        Date today = Calendar.getInstance().getTime();
+        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm:ss");
+        time = formatter.format(today);
+    }
+
+
 
 }
