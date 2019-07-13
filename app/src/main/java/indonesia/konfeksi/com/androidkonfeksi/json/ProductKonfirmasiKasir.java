2ff619dfa17bdca0a2f6696f77042349cebf9139
TryLuthfi <ty01091990@gmail.com>
2019-07-13 09:15:46 +0700

    anu

---
+++ app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/json/ProductKonfirmasiKasir.java
@@ -0,0 +1,214 @@
+package indonesia.konfeksi.com.androidkonfeksi.json;
+
+
+public class ProductKonfirmasiKasir {
+
+    private String id_penjualan;
+    private String id_karyawan;
+    private String id_pelanggan;
+    private String date;
+    private String time;
+    private String no_faktur;
+    private String no_nota;
+    private String metode_pembayaran;
+    private String diskon_persen;
+    private String diskon_rupiah;
+    private String total_harga;
+    private String biaya;
+    private String selisih;
+    private String status;
+    private String kode_karyawan;
+    private String username;
+    private String password;
+    private String nama;
+    private String alamat;
+    private String kota;
+    private String negara;
+    private String kode_pos;
+    private String no_telp;
+    private String email;
+    private String date_input;
+    private String date_edit;
+    private String token;
+    private String id_posisi;
+    private String kode_pelanggan;
+    private String nama_toko;
+    private String no_telp2;
+    private String no_telp3;
+    private String catatan;
+    private String nama_karyawan;
+
+    public ProductKonfirmasiKasir(String id_penjualan, String id_karyawan, String id_pelanggan, String date, String time, String no_faktur, String no_nota, String metode_pembayaran, String diskon_persen, String diskon_rupiah, String total_harga, String biaya, String selisih, String status, String kode_karyawan, String username, String password, String nama, String alamat, String kota, String negara, String kode_pos, String no_telp, String email, String date_input, String date_edit, String token, String id_posisi, String kode_pelanggan, String nama_toko, String no_telp2, String no_telp3, String catatan, String nama_karyawan) {
+        this.id_penjualan = id_penjualan;
+        this.id_karyawan = id_karyawan;
+        this.id_pelanggan = id_pelanggan;
+        this.date = date;
+        this.time = time;
+        this.no_faktur = no_faktur;
+        this.no_nota = no_nota;
+        this.metode_pembayaran = metode_pembayaran;
+        this.diskon_persen = diskon_persen;
+        this.diskon_rupiah = diskon_rupiah;
+        this.total_harga = total_harga;
+        this.biaya = biaya;
+        this.selisih = selisih;
+        this.status = status;
+        this.kode_karyawan = kode_karyawan;
+        this.username = username;
+        this.password = password;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.kota = kota;
+        this.negara = negara;
+        this.kode_pos = kode_pos;
+        this.no_telp = no_telp;
+        this.email = email;
+        this.date_input = date_input;
+        this.date_edit = date_edit;
+        this.token = token;
+        this.id_posisi = id_posisi;
+        this.kode_pelanggan = kode_pelanggan;
+        this.nama_toko = nama_toko;
+        this.no_telp2 = no_telp2;
+        this.no_telp3 = no_telp3;
+        this.catatan = catatan;
+        this.nama_karyawan = nama_karyawan;
+    }
+
+    public String getId_penjualan() {
+        return id_penjualan;
+    }
+
+    public String getId_karyawan() {
+        return id_karyawan;
+    }
+
+    public String getId_pelanggan() {
+        return id_pelanggan;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getNo_faktur() {
+        return no_faktur;
+    }
+
+    public String getNo_nota() {
+        return no_nota;
+    }
+
+    public String getMetode_pembayaran() {
+        return metode_pembayaran;
+    }
+
+    public String getDiskon_persen() {
+        return diskon_persen;
+    }
+
+    public String getDiskon_rupiah() {
+        return diskon_rupiah;
+    }
+
+    public String getTotal_harga() {
+        return total_harga;
+    }
+
+    public String getBiaya() {
+        return biaya;
+    }
+
+    public String getSelisih() {
+        return selisih;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getKode_karyawan() {
+        return kode_karyawan;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getKota() {
+        return kota;
+    }
+
+    public String getNegara() {
+        return negara;
+    }
+
+    public String getKode_pos() {
+        return kode_pos;
+    }
+
+    public String getNo_telp() {
+        return no_telp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDate_input() {
+        return date_input;
+    }
+
+    public String getDate_edit() {
+        return date_edit;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getId_posisi() {
+        return id_posisi;
+    }
+
+    public String getKode_pelanggan() {
+        return kode_pelanggan;
+    }
+
+    public String getNama_toko() {
+        return nama_toko;
+    }
+
+    public String getNo_telp2() {
+        return no_telp2;
+    }
+
+    public String getNo_telp3() {
+        return no_telp3;
+    }
+
+    public String getCatatan() {
+        return catatan;
+    }
+
+    public String getNama_karyawan() {
+        return nama_karyawan;
+    }
+}
+
