09b791cc4ecf909c65a6f705a7c1f2802b6431c2
TryLuthfi <ty01091990@gmail.com>
2019-07-16 13:58:26 +0700

    HADA

--- app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/activity/Kasir.java
+++ app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/activity/Kasir.java
@@ -1,24 +1,18 @@
 package indonesia.konfeksi.com.androidkonfeksi.activity;
 
-import android.graphics.Color;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.FrameLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import indonesia.konfeksi.com.androidkonfeksi.R;
 import indonesia.konfeksi.com.androidkonfeksi.fragment.HistoryPembelian;
 import indonesia.konfeksi.com.androidkonfeksi.fragment.HistoryPenjualan;
