7b9aa2c2e1c3e2772bc2e01817c7ccbe34829f74
witaf76 <witaf76@gmail.com>
2019-07-19 10:37:16 +0700

    push notification firebase

--- app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/activity/notifikasi.java
+++ app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/activity/notifikasi.java
@@ -1,18 +1,176 @@
 package indonesia.konfeksi.com.androidkonfeksi.activity;
 
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.text.Html;
 
 import indonesia.konfeksi.com.androidkonfeksi.R;
+import indonesia.konfeksi.com.androidkonfeksi.json.Notif;
 
-public class notifikasi extends AppCompatActivity {
-    public TextView notification;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.notifikasi);
-        notification= findViewById(R.id.teks);
+public class notifikasi  {
+
+    private static final int NOTIFICATION_ID = 200;
+    private static final String PUSH_NOTIFICATION = "pushNotification";
+    private static final String CHANNEL_ID = "indonesia.konfeksi.com.androidkonfeksi.test";
+    private static final String CHANNEL_NAME = "Notification";
+    private static final String URL = "url";
+    private static final String ACTIVITY = "activity";
+    Map<String, Class> activityMap = new HashMap<>();
+    private Context mContext;
+
+    public  notifikasi(Context mContext)  {
+        this.mContext = mContext;
+        //mapping activity
+        activityMap.put("handleNotifikasi", handleNotifikasi.class);
+
+    }
+
+    public void displayNotification(Notif notif, Intent resultIntent) {
+        {
+            String message = notif.getMessage();
+            String title = notif.getTitle();
+            String iconUrl = notif.getIconUrl();
+            String action = notif.getAction();
+            String destination = notif.getActionDestination();
+            Bitmap iconBitMap = null;
+            if (iconUrl != null) {
+                iconBitMap = getBitmapFromURL(iconUrl);
+            }
+            final int icon = R.drawable.ic_notification;
+
+            PendingIntent resultPendingIntent;
+
+            if (URL.equals(action)) {
+                Intent notificationIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(destination));
+
+                resultPendingIntent = PendingIntent.getActivity(mContext, 0, notificationIntent, 0);
+            } else if (ACTIVITY.equals(action) && activityMap.containsKey(destination)) {
+                resultIntent = new Intent(mContext, activityMap.get(destination));
+
+                resultPendingIntent =
+                        PendingIntent.getActivity(
+                                mContext,
+                                0,
+                                resultIntent,
+                                PendingIntent.FLAG_CANCEL_CURRENT
+                        );
+            } else {
+                resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                resultPendingIntent =
+                        PendingIntent.getActivity(
+                                mContext,
+                                0,
+                                resultIntent,
+                                PendingIntent.FLAG_CANCEL_CURRENT
+                        );
+            }
+
+
+            final NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+                    mContext, CHANNEL_ID);
+
+            Notification notification;
+
+            if (iconBitMap == null) {
+                //small icon
+                NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+
+                inboxStyle.addLine(message);
+                notification = mBuilder.setSmallIcon(icon)
+                        .setTicker(title)
+                        .setWhen(System.currentTimeMillis())
+                        .setAutoCancel(true)
+                        .setContentTitle(title)
+                        .setContentIntent(resultPendingIntent)
+                        .setStyle(inboxStyle)
+                        .setSmallIcon(R.drawable.ic_notification)
+                        .setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(), icon))
+                        .setContentText(message)
+                        .build();
+
+            } else {
+                //big icon
+                NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat.BigPictureStyle();
+                bigPictureStyle.setBigContentTitle(title);
+                bigPictureStyle.setSummaryText(Html.fromHtml(message).toString());
+                bigPictureStyle.bigPicture(iconBitMap);
+                notification = mBuilder.setSmallIcon(icon)
+                        .setTicker(title)
+                        .setWhen(System.currentTimeMillis())
+                        .setAutoCancel(true)
+                        .setContentTitle(title)
+                        .setContentIntent(resultPendingIntent)
+                        .setStyle(bigPictureStyle)
+                        .setSmallIcon(R.drawable.ic_notification)
+                        .setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(), icon))
+                        .setContentText(message)
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .build();
+            }
+
+            NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            //cek kompatibel
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                        CHANNEL_NAME,
+                        NotificationManager.IMPORTANCE_DEFAULT);
+                channel.setDescription("Android Konfeksi");
+                channel.enableLights(true);
+                channel.setLightColor(android.R.color.holo_blue_light);
+                channel.setVibrationPattern(new long[]{0,1000,500,1000});
+                channel.enableLights(true);
+                notificationManager.createNotificationChannel(channel);
+
+            }
+            notificationManager.notify(NOTIFICATION_ID, notification);
+        }
+    }
+
+
+    private Bitmap getBitmapFromURL(String strURL) {
+        try {
+            URL url = new URL(strURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            return BitmapFactory.decodeStream(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+ //notif ringtone
+    public void playNotificationSound() {
+        try {
+            Uri alarmSound = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE
+                    + "://" + mContext.getPackageName() + "/raw/notification");
+            Ringtone r = RingtoneManager.getRingtone(mContext, alarmSound);
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/indonesia/konfeksi/com/androidkonfeksi/json/Notif.java
@@ -0,0 +1,55 @@
+package indonesia.konfeksi.com.androidkonfeksi.json;
+
+public class Notif {
+
+    private String title;
+    private String message;
+    private String iconUrl;
+    private String action;
+    private String actionDestination;
+
+    public Notif() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getActionDestination() {
+        return actionDestination;
+    }
+
+    public void setActionDestination(String actionDestination) {
+        this.actionDestination = actionDestination;
+    }
+}
+
